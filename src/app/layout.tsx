24b6b909fb591bdafac1fcd4e5771db917bed0ef
Ogo-Ayorinde Shekinah <A.shekinah@carestaffing-solution.com>
2025-06-13 15:33:49 +0100

    Meta Twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
       "I create high-performing, visually engaging websites using React and Tailwind. Let’s build something impactful together.",
     images: [
       {
-        url: "https://myportfolio-neon-alpha.vercel.app/og-image.png",
+        url: "https://myportfolio-neon-alpha.vercel.app/twitter-image.png",
         width: 1200,
         height: 630,
         alt: "Shekinah Front-End Portfolio",
